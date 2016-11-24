9d1fe7a4444e01169e3e66d83a7a19153b0e73c9
Marius Melzer <marius@rasumi.net>
2016-11-24 23:45:10 +0100

    add termination to TwoWindowTerminateOperator and facilitate construction

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/operators/windowing/TwoWindowTerminateOperator.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/operators/windowing/TwoWindowTerminateOperator.java
@@ -1,12 +1,11 @@
 package org.apache.flink.streaming.runtime.operators.windowing;
 
 import org.apache.flink.annotation.Internal;
-import org.apache.flink.api.common.functions.TerminationFunction;
 import org.apache.flink.api.common.state.AppendingState;
 import org.apache.flink.api.common.state.StateDescriptor;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.runtime.state.StateInitializationContext;
+import org.apache.flink.streaming.api.functions.windowing.TerminationFunction;
 import org.apache.flink.streaming.api.graph.StreamConfig;
 import org.apache.flink.streaming.api.operators.*;
 import org.apache.flink.streaming.api.watermark.Watermark;
@@ -19,53 +18,33 @@ import org.apache.flink.streaming.runtime.streamrecord.LatencyMarker;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 import org.apache.flink.streaming.runtime.tasks.OperatorStateHandles;
 import org.apache.flink.streaming.runtime.tasks.StreamTask;
+import org.apache.flink.types.Either;
 
-import java.io.Serializable;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Internal
-public class TwoWindowTerminateOperator<K1, K2, IN1, IN2, ACC1, ACC2, OUT, W1 extends Window, W2 extends Window>
-	extends AbstractStreamOperator<OUT>
-	implements TwoInputStreamOperator<IN1, IN2, OUT> {
+public class TwoWindowTerminateOperator<K, IN1, IN2, ACC1, ACC2, R, S, W1 extends Window, W2 extends Window>
+	extends AbstractStreamOperator<Either<R,S>>
+	implements TwoInputStreamOperator<IN1, IN2, Either<R,S>> {
 
-	WindowOperator<K1, IN1, ACC1, OUT, W1> winOp1;
-	WindowOperator<K2, IN2, ACC2, OUT, W2> winOp2;
-
+	WindowOperator<K, IN1, ACC1, Either<R,S>, W1> winOp1;
+	WindowOperator<K, IN2, ACC2, Either<R,S>, W2> winOp2;
 	TerminationFunction terminationFunction;
 
 	private StreamInputProgressHandler progressHandler;
+	private Map<List<Long>,Long> iterationIndices = new HashMap<>();
 
-	public TwoWindowTerminateOperator(WindowAssigner<? super IN1, W1> windowAssigner1,
-									  WindowAssigner<? super IN1, W1> windowAssigner2,
-									  TypeSerializer<W1> windowSerializer1,
-									  TypeSerializer<W2> windowSerializer2,
-									  KeySelector<IN1, K1> keySelector1,
-									  KeySelector<IN2, K2> keySelector2,
-									  TypeSerializer<K1> keySerializer1,
-									  TypeSerializer<K2> keySerializer2,
-									  StateDescriptor<? extends AppendingState<IN1, ACC1>, ?> windowStateDescriptor1,
-									  StateDescriptor<? extends AppendingState<IN2, ACC2>, ?> windowStateDescriptor2,
-									  InternalWindowFunction<ACC1, OUT, K1, W1> windowFunction1,
-									  InternalWindowFunction<ACC2, OUT, K2, W2> windowFunction2,
-									  Trigger<? super IN1, ? super W1> trigger1,
-									  Trigger<? super IN2, ? super W2> trigger2,
-									  long allowedLateness,
-									  TerminationFunction terminationFunction) {
-		// TODO combine windowFunction1 and 2 to one (Rich)Window(Terminate)Function and wrap them!
-
-		winOp1 = new WindowOperator(windowAssigner1, windowSerializer1, keySelector1, keySerializer1,
-			windowStateDescriptor1, windowFunction1, trigger1, allowedLateness);
-
-		winOp2 = new WindowOperator(windowAssigner2, windowSerializer2, keySelector2, keySerializer2,
-			windowStateDescriptor2, windowFunction2, trigger2, allowedLateness);
-
+	public TwoWindowTerminateOperator(WindowOperator winOp1, WindowOperator winOp2, TerminationFunction terminationFunction) {
+		this.winOp1 = winOp1;
+		this.winOp2 = winOp2;
 		this.terminationFunction = terminationFunction;
-
 		progressHandler = new StreamInputProgressHandler(2);
 	}
 
 	@Override
-	public void setup(StreamTask<?, ?> containingTask, StreamConfig config, Output<StreamRecord<OUT>> output) {
+	public void setup(StreamTask<?, ?> containingTask, StreamConfig config, Output<StreamRecord<Either<R,S>>> output) {
 		// setup() both with own output
 		winOp1.setup(containingTask, config, output);
 		winOp2.setup(containingTask, config, output);
@@ -101,9 +80,12 @@ public class TwoWindowTerminateOperator<K1, K2, IN1, IN2, ACC1, ACC2, OUT, W1 ex
 	}
 
 	public void processElement1(StreamRecord<IN1> element) throws Exception {
+		// TODO include iteration index somehow
 		winOp1.processElement(element);
 	}
 	public void processElement2(StreamRecord<IN2> element) throws Exception {
+		// TODO Only do this if the context still exists??
+		// TODO include iteration index somehow
 		winOp2.processElement(element);
 	}
 
@@ -111,14 +93,33 @@ public class TwoWindowTerminateOperator<K1, K2, IN1, IN2, ACC1, ACC2, OUT, W1 ex
 		//watermarks need to be treated together because we're unioning here!
 		Watermark next = progressHandler.getNextWatermark(mark, 0);
 		if(next != null) {
-			winOp1.processWatermark(mark);
+			iterationIndices.put(mark.getContext(), mark.getTimestamp());
+			winOp2.processWatermark(mark);
+
+			if(terminationFunction.terminate()) {
+				terminateIteration(mark);
+			}
 		}
 	}
 	public void processWatermark2(Watermark mark) throws Exception {
 		//watermarks need to be treated together because we're unioning here!
 		Watermark next = progressHandler.getNextWatermark(mark, 1);
 		if(next != null) {
-			winOp2.processWatermark(mark);
+			if(mark.getTimestamp() == Long.MAX_VALUE || terminationFunction.terminate()) {
+				terminateIteration(mark);
+			} else {
+				// TODO only to Feedback?
+				winOp2.processWatermark(mark);
+				iterationIndices.put(mark.getContext(), mark.getTimestamp());
+			}
+		}
+	}
+
+	private void terminateIteration(Watermark mark) {
+		if(iterationIndices.get(mark.getContext()) != null) {
+			terminationFunction.onTermination();
+			iterationIndices.remove(mark.getContext());
+			// TODO send watermark to output!
 		}
 	}
 
