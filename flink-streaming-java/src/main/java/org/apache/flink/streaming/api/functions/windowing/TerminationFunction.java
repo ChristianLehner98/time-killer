2a817796cc999b7b83561d5b392502bc4fc4dbe7
Marius Melzer <marius@rasumi.net>
2016-11-27 19:39:07 +0100

    add parameters to the TerminationFunction interface functions

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/windowing/TerminationFunction.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/windowing/TerminationFunction.java
@@ -2,11 +2,12 @@ package org.apache.flink.streaming.api.functions.windowing;
 
 import org.apache.flink.annotation.Public;
 import org.apache.flink.api.common.functions.Function;
+import org.apache.flink.util.Collector;
 
 import java.io.Serializable;
 
 @Public
-public interface TerminationFunction<T, O> extends Function, Serializable {
-	boolean terminate();
-	void onTermination();
+public interface TerminationFunction<OUT> extends Function, Serializable {
+	boolean terminate(int i);
+	void onTermination(int i, Collector<OUT> out) throws Exception;
 }
